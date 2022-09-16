bdd0a2f81287607c2dc69b7b8111225e4f1496a5
Thiago Munich <thiagomunich@gmail.com>
2022-09-16 18:20:41 -0300

    feat: creating section below header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react"
 
 import "./App.css"
-import Header from "./components/Header/Header"
+import { FaBell } from "react-icons/fa"
 
 function App() {
   return (
     <>
       <div className="container">
-        <header>header</header>
-        <section>section</section>
+        <header>
+          <img src="/assets/images/govLogo.png" alt="gov.br logo" />
+
+          <FaBell size={32} color="#7E7E7E" style={{ marginLeft: "auto" }} />
+
+          <img src="/assets/images/profilePicture.jpg" alt="profile" />
+        </header>
+
+        <section>
+          <h3>Olá, Thiago</h3>
+          <p>
+            SUA CONTA É NÍVEL <strong>OURO</strong>
+          </p>
+          <div style={{ display: "flex", margin: 8 }}>
+            <span className="teste"></span>
+            <span className="teste"></span>
+            <span className="teste"></span>
+          </div>
+        </section>
+
         <main>main</main>
+
         <footer>footer</footer>
       </div>
     </>
