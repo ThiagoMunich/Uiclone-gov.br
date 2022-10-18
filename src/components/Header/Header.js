3fddef7f722b450b3b708d1112db276922ef08bb
Thiago Munich <thiagomunich@gmail.com>
2022-10-18 18:41:15 -0300

    feat: fixing @media and adding sweetalert

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import Swal from "sweetalert2"
+
 import "./styles.css"
 
 import { FaBell } from "react-icons/fa"
@@ -9,7 +11,17 @@ export default function Header() {
     <>
       <img src="/assets/images/govLogo.png" alt="gov.br logo" />
 
-      <div style={{ display: "flex", marginLeft: "auto" }}>
+      <div
+        style={{ display: "flex", marginLeft: "auto" }}
+        onClick={() =>
+          Swal.fire({
+            icon: "info",
+            title: "Hi there!",
+            text: "This is just an UI clone and this alert is the only interaction within the page.",
+            confirmButtonText: "Ok, got it",
+          })
+        }
+      >
         <FaBell size={26} color="#7E7E7E" className="bell" />
 
         <i className="bellBadge" />
